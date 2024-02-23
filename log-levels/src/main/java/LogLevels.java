f6d72cc0f541bae7276f266071ab8e1940b090ec
RK <nahsirkivar@gmail.com>
2024-02-23 11:01:19 +0100

    remove unnecessary allocation, style change for calling static methods

--- log-levels/src/main/java/LogLevels.java
+++ log-levels/src/main/java/LogLevels.java
@@ -5,12 +5,10 @@ public class LogLevels {
     }
 
     public static String logLevel(String logLine) {
-        String bracketed = logLine.split(":")[0].trim();
-
-        return bracketed.substring(1, bracketed.length() - 1).toLowerCase();
+        return logLine.split("]:")[0].trim().substring(1).toLowerCase();
     }
 
     public static String reformat(String logLine) {
-        return String.format("%s (%s)", LogLevels.message(logLine), LogLevels.logLevel(logLine));
+        return String.format("%s (%s)", message(logLine), logLevel(logLine));
     }
 }
